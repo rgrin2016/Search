4927f50471bd0f8ba8ef340c26784d4534b19370
rgrin2016 <r.grin.444@gmail.com>
2023-04-12 20:40:38 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,18 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "Sklad") or die("Error");
-
-$query = mysqli_query($connect, "SELECT * FROM Works");
-
-/* while($row = mysqli_fetch_assoc($query)) echo "<h1>".$row['title']."</h1><p>".$row['text']."</p><br>"; */
-?>
-
-
-
 <!DOCTYPE html>
-<html>
-    <head>
-    <title>PHP SEARCHING</title>
-    </head>    
-    <body>
-    <form method="post">
-        <input type="text" name="search" class="search">
-        <input type="submit" name="submit" value="поиск">
-    </form>
-    <hr>
-    <?php
-        if(isset($_POST['submit'])){
-            $search = explode(" ", $_POST['search']);
-            $count = count($search);
-            $array = array();
-            $i = 0;
-            foreach($search as $key) {
-                $i++;
-                if($i < $count) $array[] = " CONCAT (`id_works`,`name`) LIKE '%".$key."%' OR "; else $array[] = " CONCAT (`id_works`,`name`) LIKE '%".$key."%' OR ";
-            }
-            $sql = "SELECT * FROM `Works` WHERE ". implode("", $array);
-            echo $sql;
-            $query = mysqli_query($connect, $sql);
-            while($row = mysqli_fetch_assoc($query))  echo "<h1>".$row['id_works']. "</h1><p>".$row['name']."</p><br>";
-        }
-    ?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+<form method="get" action="search.php">
+	<input type="text" name="query" placeholder="Введите запрос">
+	<button type="submit">Найти</button>
+</form>
 
-    </body>
+
+</body>
 </html>
